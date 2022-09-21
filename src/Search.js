f41060935a82f765cffe506cd09bc37eb123fb3c
VBasko <106957194+VBasko@users.noreply.github.com>
2022-09-21 14:12:39 +0400

    Realized search handling

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Search() {
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}`);
+  }
+
   return (
     <header className="header">
       <div className="header__container">
-        <form>
-          <input type={search} />
+        <form onSubmit={search}>
+          <input type="search" onChange={(e) => setKeyword(e.target.value)} />
         </form>
       </div>
     </header>
