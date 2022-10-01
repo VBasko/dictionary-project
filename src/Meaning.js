45aaab61b977dec2b51e90873c65ab47093c118c
VBasko <106957194+VBasko@users.noreply.github.com>
2022-10-01 18:03:01 +0400

    Loop through all meanings, extract definitions

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,10 +1,20 @@
 import React from "react";
 
 function Meaning(props) {
+  console.log(props.meaning);
   return (
-    <div>
+    <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions[0].definition}
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>{definition.definition}</p>
+            <p>
+              <em>{definition.example}</em>
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 }
